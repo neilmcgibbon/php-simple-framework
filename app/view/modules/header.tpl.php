2cb211f4048638de9667a42f3d2d459fc01e693b
Neil McGibbon <neil@gib.so>
2013-01-11 12:07:08 +0000

    added Normalize.css

--- app/view/modules/header.tpl.php
+++ app/view/modules/header.tpl.php
@@ -4,14 +4,16 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title></title>
 
+<link rel="stylesheet" type="text/css" href="/web/css/normalize.css" />
 <?php 
 foreach ($data['stylesheets'] as $stylesheet)
 	echo '<link rel="stylesheet" type="text/css" href="' . $stylesheet . '" />' . PHP_EOL;;
 ?>
 
+<script src="/web/js/jquery-1.8.3.min.js"></script>
 <?php
 foreach ($data['javascripts'] as $script)
-	echo '<script type="text/javascript" src="' . $script . '"></script>' . PHP_EOL;;
+	echo '<script src="' . $script . '"></script>' . PHP_EOL;;
 ?>
   
 </head>
